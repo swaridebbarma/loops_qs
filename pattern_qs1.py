57bde7a58a3252386e6e86b1d1631d990428bd14
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 19:50:48 +0530

    Add files via upload

---
+++ pattern_qs1.py
@@ -0,0 +1,19 @@
+n=input("enter name:-")
+r=5
+while r>0:
+    c=0
+    while c<r:
+        print(n[c],end="")
+        c+=1
+    print()
+    r-=1
+
+n=input("enter name:-")
+r=1
+while r<=5:
+    c=0
+    while c<r:
+        print(n[c],end="")
+        c+=1
+    print()
+    r+=1
